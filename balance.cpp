bff85c8a6cdbff9815fb5651b89022c6f23a379d
sgosiaco <sgosiaco@me.com>
2016-09-30 23:45:41 -0700

    Getting there

--- balance.cpp
+++ balance.cpp
@@ -5,165 +5,120 @@
 #include <cstdlib>
 #include <cstdio>
 
-#include "CPUTimer.h"
-#include "LinkedList.h"
-#include "CursorList.h"
-#include "StackAr.h"
 #include "StackLi.h"
-#include "QueueAr.h"
-#include "SkipList.h"
-#include "vector.h"
+
 
 using namespace std;
 
-void RunListCHAR(char* filename)
+bool checkIfPair(char left, char right)
 {
-  StackLi<char> stackLi;
-  StackLi<int> count;
-
-  char temp = ' ', store = ' ', oldC = ' ';
-  int i = 1, comment = 0, asterix = 0, paren = 0, brace = 0, bracket = 0, debug = 0, line = 0, oldL = 0;
-  ifstream inf(filename);
-  while(inf.get(temp))
-  {
-    switch (temp)
-    {
-      case '\n':
-         i++;
-        break;
-      case '/':
-        comment = 1;
-        break;
-      case '*':
-        asterix = 1;
-        break;
-      case '(':
-        stackLi.push(temp);
-        count.push(i);
-        break;
-      case '{':
-        stackLi.push(temp);
-        count.push(i);
-        break;
-      case '[':
-        stackLi.push(temp);
-        count.push(i);
-        break;
-
-      case ')':
-        stackLi.push(temp);
-        count.push(i);
-        break;
-      case '}':
-        stackLi.push(temp);
-        count.push(i);
-        break;
-      case ']':
-        stackLi.push(temp);
-        count.push(i);
-        break;
-    }
-    //cout << temp;
-    //temp = *s;
-    //s[0] = ' ';
-    //i = atoi(s);
-    //cout << temp << " " << i << endl;
-  }
-  //cout << endl;
-  if(debug == 1)
-  {
-    while(stackLi.isEmpty() != 1)
-      cout << count.topAndPop() << stackLi.topAndPop();
-    cout << endl;
-  }
+  if(left == '{' && right == '}')
+    return true;
   else
-  {
-    while(!stackLi.isEmpty())
-    {
-      store = stackLi.topAndPop();
-      line = count.topAndPop();
-      cout << "c:" << store << " line:" << line << endl;
-      switch(store)
-      {
-        case '/':
-          comment = 1;
-          break;
-        case '*':
-          asterix = 1;
-          break;
-        case '(':
-          if(paren != 1)
-          {
-            cout << "Unmatched " << oldC << " on line #" << oldL << endl;
-            return;
-          }
-          else
-            paren = 0;
-          break;
-        case '{':
-          if(brace != 1)
-          {
-            cout << "Unmatched " << oldC << " on line #" << oldL << endl;
-            return;
-          }
-          else
-            brace = 0;
-          break;
-        case '[':
-          if(bracket != 1)
-          {
-            cout << "Unmatched " << oldC << " on line #" << oldL << endl;
-            return;
-          }
-          else
-            bracket = 0;
-          break;
-
-        case ')':
-          paren = 1;
-          break;
-        case '}':
-          if(brace == 1)
-          {
-            cout << "Unmatched " << oldC << " on line #" << oldL << endl;
-            return;
-          }
-          brace = 1;
-          break;
-        case ']':
-          bracket = 1;
-          break;
-      }
-      oldC = store;
-      oldL = line;
-    }
-  }
-
-  cout << "OK" << endl;
-  inf.close();
+    if(left == '(' && right == ')')
+      return true;
+    else
+      if(left == '[' && right == ']')
+        return true;
+      else
+        if(left == '*' && right == '/')
+          return true;
+  return false;
 }
 
 void RunListString(char* filename)
 {
-  StackLi<char*> stackLi;
+  StackLi<char> stackLi;
   StackLi<int> count;
 
-  char temp = ' ';
-  char* s = new char[256];
-  int i = 0;
+  char s = ' ';
+  int i = 1, comment = 0;
   ifstream inf(filename);
-  while(inf.getline(s, 256))
+  while(inf.get(s))
   {
-    cout << s << endl;
-    //temp = *s;
-    //s[0] = ' ';
-    //i = atoi(s);
-    stackLi.push(s);
-    count.push(++i);
-    //cout << temp << " " << i << endl;
+    //cout << "c:" << s << ' ' << comment << endl;
+    if(s == '\0')
+      return;
+    if(s == '\n')
+      i++;
+    if(s == '(' || s == '{' || s == '[' || s == '/')
+    {
+      if(comment == 0)
+      {
+        if(s == '/')
+        {
+          inf.get(s);
+          if(s == '*')
+          {
+            comment = 1;
+            stackLi.push(s);
+            count.push(i);
+          }
+          else
+          {
+            if(s == '(' || s == '{' || s == '[')
+            {
+              stackLi.push(s);
+              count.push(i);
+            }
+          }
+        }
+        stackLi.push(s);
+        count.push(i);
+      }
+    }
+    else
+    {
+      if(s == ')' || s == '}' || s == ']' || s == '*')
+      {
+        if(s == '*')
+        {
+          inf.get(s);
+          if(s == '/')
+          {
+            if(comment == 1)
+            {
+              comment = 0;
+              if(stackLi.isEmpty() || !checkIfPair(stackLi.top(), s))
+              {
+                cout << "Unmatched " << s << " on line #" << i << endl;
+                return;
+              }
+              else
+              {
+                stackLi.pop();
+                count.pop();
+              }
+            }
+            else
+            {
+              cout << "Unmatched */ on line #" << i << endl;
+              return;
+            }
+          }
+        }
+        if(comment == 0 && (s == ')' || s == '}' || s == ']'))
+        {
+          cout << stackLi.top() << ' ' << s << endl;
+          if(stackLi.isEmpty() || !checkIfPair(stackLi.top(), s))
+          {
+            cout << "Unmatched " << s << " on line #" << i << endl;
+            return;
+          }
+          else
+          {
+            stackLi.pop();
+            count.pop();
+          }
+        }
+      }
+    }
   }
-  while(count.isEmpty() != 1)
-    cout << count.topAndPop() << ' ' << stackLi.topAndPop() << endl;
+  if(comment == 1)
+    cout << "Unmatched /* on line #" << count.topAndPop() << endl;
+  if(stackLi.isEmpty())
+    cout << "OK" << endl;
+
   inf.close();
 }
 
@@ -173,5 +128,5 @@ int main(int argc, char** argv)
 {
   char* filename = *(argv + 1);
   if(argc > 1)
-    RunListCHAR(filename);
+    RunListString(filename);
 }
