1b74e6cb2c992c94579f4fa28601ad2bebae40b5
sgosiaco <sgosiaco@me.com>
2016-10-03 13:56:57 -0700

    removed some usued includes and also did bags.cpp

---
+++ bags.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+
+#include "StackLi.h"
+#include "QueueAr.h"
+
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  short num;
+  StackLi<Queue<int> *> stack;
+  if(argc > 1)
+  {
+    ifstream inf(*(argv + 1));
+    argc = atoi(*(argv + 2));
+    cout << argc << endl;
+    while(inf >> num)
+    {
+      if(stack.isEmpty() || stack.top()->isFull())
+      {
+        cout << "new ";
+        stack.push(new Queue<int> (argc) );
+      }
+
+      cout << num << ' ';
+      stack.top()->enqueue(num);
+    }
+    cout << endl;
+    inf.close();
+    while(!stack.isEmpty())
+    {
+      while(!stack.top()->isEmpty())
+      {
+        cout << stack.top()->dequeue() << ' ';
+      }
+      stack.pop();
+    }
+    cout << endl;
+  }
+}
