d9ac7a2d8ff37f89458b4caefb99eec902835c48
sgosiaco <sgosiaco@me.com>
2016-10-25 13:14:23 -0700

    temp

--- run3.sh
+++ run3.sh
@@ -4,14 +4,32 @@ echo This script assumes that your timetest program is named a.out.
 echo The times of the three runs for each ADT are placed in the file \"results\"
 echo "File ADT# Time#1  Ignore" > results
 
-set ADT = 11
+set ADT = 8
 
-while ($ADT < 12)
+while ($ADT < 15)
   set fileNum = 1
-  while ($fileNum < 2)
+  while ($fileNum < 5)
     echo File{$fileNum}.dat > tt.tmp
     echo Running File{$fileNum}.dat once for ADT $ADT
     echo $ADT >> tt.tmp
+
+    if( $ADT == 10 ) then
+      echo 3 >> tt.tmp
+      echo 1 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 3 >> tt.tmp
+      echo 200 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 1000 >> tt.tmp
+      echo 2 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 1000 >> tt.tmp
+      echo 200 >> tt.tmp
+      echo M,L 3,1  3,200 1000,2  1000,200
+    fi
 
     if ( $ADT == 11 ) then
       echo 1000000 >> tt.tmp
@@ -30,6 +48,42 @@ while ($ADT < 12)
       echo Load factor 0.5 1 10 100 1000 >> results
       echo HT Size 1000000 500000 50000 5000 500 >> results
     endif
+
+    if ( $ADT == 12 ) then
+      echo 5000000 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 2000000 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 1000000 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 500000 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 250000 >> tt.tmp
+      echo Load factor 0.1 .25 .5 1 2 >> results
+      echo HT Size 5000000 2000000 1000000 500000 250000 >> results
+    fi
+
+    if( $ADT == 14 ) then
+      echo 5000000 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 2000000 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 1000000 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 500000 >> tt.tmp
+
+      echo $ADT >> tt.tmp
+      echo 250000 >> tt.tmp
+      echo Load factor 0.1 .25 .5 1 2 >> results
+      echo HT Size 5000000 2000000 1000000 500000 250000 >> results
+    fi
 
     echo 0 >> tt.tmp
     echo -n File{$fileNum}  $ADT "  " >> results
